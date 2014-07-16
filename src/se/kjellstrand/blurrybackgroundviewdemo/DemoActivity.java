0d62cf0d85f436d8daa0844a3413c7446f84d63f
Carl-Emil Kjellstrand <carl-emil.kjellstrand@sonymobile.com>
2014-07-16 14:37:33 +0200

    first semi working prototype

--- src/se/kjellstrand/blurrybackgroundviewdemo/DemoActivity.java
+++ src/se/kjellstrand/blurrybackgroundviewdemo/DemoActivity.java
@@ -1,11 +1,16 @@
 
 package se.kjellstrand.blurrybackgroundviewdemo;
 
+import java.util.concurrent.atomic.AtomicBoolean;
+
 import android.app.Activity;
 import android.os.Bundle;
 import android.view.View;
+import android.view.View.OnClickListener;
 
 public class DemoActivity extends Activity {
+
+    private AtomicBoolean isShowingDetails = new AtomicBoolean(false);
 
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -13,5 +18,26 @@ public class DemoActivity extends Activity {
         View view = View.inflate(this, R.layout.activity_demo, null);
         setContentView(view);
 
+        findViewById(R.id.imageView1).setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                BlurredBackgroundLayout bluredBackgroundLayout =
+                        (BlurredBackgroundLayout) findViewById(R.id.launch_parent);
+                View detailsView = getLayoutInflater().inflate(R.layout.details_view,
+                        bluredBackgroundLayout, false);
+                bluredBackgroundLayout.setInnerView(DemoActivity.this, detailsView, view);
+                bluredBackgroundLayout.runAnimations(DemoActivity.this, isShowingDetails);
+            }
+        });
+    }
+
+    @Override
+    public void onBackPressed() {
+        if (isShowingDetails.get()) {
+            BlurredBackgroundLayout v = (BlurredBackgroundLayout) findViewById(R.id.launch_parent);
+            v.forceCloseDetailsView();
+        } else {
+            super.onBackPressed();
+        }
     }
 }
